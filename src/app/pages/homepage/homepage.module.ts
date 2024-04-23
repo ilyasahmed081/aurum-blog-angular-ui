84a0f4e2349e6a8f9ce540e2d91868bfde0cec79
ilyasahmed081 <ilyasahmed081@gmail.com>
2024-04-23 17:48:48 +0500

    blog cards fixed

--- src/app/pages/homepage/homepage.module.ts
+++ src/app/pages/homepage/homepage.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { HomepageComponent } from './homepage.component';
-
+import { CardComponent } from '../../components/card/card.component';
 
 @NgModule({
-  declarations: [
-    HomepageComponent
-  ],
-  imports: [
-    CommonModule,
-    HomepageRoutingModule
-  ]
+  declarations: [HomepageComponent],
+  imports: [CommonModule, HomepageRoutingModule, CardComponent],
 })
-export class HomepageModule { }
+export class HomepageModule {}
